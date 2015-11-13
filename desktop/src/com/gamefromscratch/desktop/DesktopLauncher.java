1464e5cc484a01cff1e8c4f720c56a684764f4c9
stephcornick <stephc88733@aol.com>
2015-11-13 15:54:15 -0500

    Initial commit

---
+++ desktop/src/com/gamefromscratch/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gamefromscratch.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gamefromscratch.Physics4;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Physics4(), config);
+	}
+}
